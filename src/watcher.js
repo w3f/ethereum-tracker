cfa9f3d8c5159345a117949de392898cde843066
Anson <lautszhinganson@gmail.com>
2019-08-15 03:46:30 +0800

    refactor

--- src/watcher.js
+++ src/watcher.js
@@ -34,7 +34,6 @@ function watchTokenContractEvent() {
     amountTransfer.observe( { from, to }, parseFloat(value)  / 1000)
 
     console.log('Transaction hash is: ', result.transactionHash)      
-    // Initiate transaction confirmation
     confirmEtherTransaction(result.transactionHash)
     return
   })
@@ -47,52 +46,19 @@ function watchClaimContractEvent() {
       (error, result) => { if (error) console.log(error) }
     )
 
-    claimContract.events.Claimed(async (error, result) => {
-      // console.log('Claimed event :', result)
-      // Initiate transaction confirmation
-      confirmEtherTransaction(result.transactionHash)
-
-      if (error) {
+    claimContract.events.allEvents({
+      topics: 
+      [  // Amended, Vested, NewOwner
+        ['0x385f6a4b73038a5f14e3482732f99dde086b6fd0930af57604250b72726f4392',
+        '0x00d5958799b183a7b738d3ad5e711305293dd5076a37a4e3b7e6611dea6114f3',
+        '0x70aea8d848e8a90fb7661b227dc522eb6395c3dac71b63cb59edd5c9899b2364']
+      ]
+    },(error, result) => {
+      if (!error) {
+        confirmEtherTransaction(result.transactionHash)
+      } else {
         console.log(error)
-        return
       }
-      return
-    })
-
-    claimContract.events.Amended(async (error, result) => {
-      // console.log('Amended event :', result)
-      // Initiate transaction confirmation
-      confirmEtherTransaction(result.transactionHash)
-
-      if (error) {
-        console.log(error)
-        return
-      }
-      return
-    })
-
-    claimContract.events.Vested(async (error, result) => {
-      // console.log('Vested event :', result)
-      // Initiate transaction confirmation
-      confirmEtherTransaction(result.transactionHash)
-
-      if (error) {
-        console.log(error)
-        return
-      }
-      return
-    })
-
-    claimContract.events.NewOwner(async (error, result) => {
-      // console.log('NewOwner event :', result)
-      // Initiate transaction confirmation
-      confirmEtherTransaction(result.transactionHash)
-
-      if (error) {
-        console.log(error)
-        return
-      }
-      return
     })
 }
 
